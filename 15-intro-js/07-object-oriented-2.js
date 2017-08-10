0a49cc2cf89a943c76761c3e99a9ebfbf8a96f39
Tim Campbell <timothylevicampbell@gmail.com>
2017-08-10 10:51:53 -0400

    Add lecture notes for object-oriented part 2

---
+++ 15-intro-js/07-object-oriented-2.js
@@ -0,0 +1,13 @@
+/**
+
+  * Review
+    * Object Constructor syntax
+    * Class syntax
+    * Prototypes
+  * Instance and classes
+    * Methods
+    * Variables
+    * Closures (private variables)
+    * Object relations
+
+**/
