775f52471ff66fed295b054b1399ee12460f07bf
Tim Campbell <timothylevicampbell@gmail.com>
2017-08-10 13:10:28 -0400

    Update lecture code from object-oriented-2

--- 15-intro-js/07-object-oriented-2.js
+++ 15-intro-js/07-object-oriented-2.js
@@ -1,7 +1,6 @@
 /**
-
   * Review
-    * Object Constructor syntax
+    * Function Constructor syntax
     * Class syntax
     * Prototypes
   * Instance and classes
@@ -9,5 +8,65 @@
     * Variables
     * Closures (private variables)
     * Object relations
-
 **/
+
+// cookie                   ['flour', 'sugar', 'butter', 'eggs']
+// cherry pie               ['cherries', 'flour', 'butter', 'sugar']
+// lemon meringue           [egg whites, lemon, sugar]
+// peanut butter mousse     [peanut butter, cream]
+
+// OLD WAY – Function Constructor
+
+// Dessert
+// create a function that returns a dessert object with a name and a list of ingredients
+// function Dessert(name, ingredients) {
+//   this.name = name
+//   this.ingredients = ingredients
+// }
+//
+// function prepare() {
+//   console.log(`Preparing ${this.name}`);
+// }
+// prepare()
+//
+// // Instance method
+// Dessert.prototype.cook = function() {
+//   console.log(`${this.name} is cooked!`);
+// }
+//
+// // cookie.cook()
+//
+// // Class methods
+// Dessert.buyGroceries = function() {
+//   console.log('buying groceries')
+// }
+
+// Dessert.buyGroceries()
+
+
+//  NEW WAY – Class syntax
+
+
+class Dessert {
+
+  constructor(name, ingredients) {
+    this.id = 0
+    this.name = name
+    this.ingredients = ingredients
+    Dessert.all().push(this)
+  }
+
+  cook() {
+    console.log(`${this.name} is cooked!`);
+  }
+
+  static buyGroceries() {
+    console.log('buying groceries')
+  }
+
+  // static last() {
+  // }
+
+  static all() {
+  }
+}
